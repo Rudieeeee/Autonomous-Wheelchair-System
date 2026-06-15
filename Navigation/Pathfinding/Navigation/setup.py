a2e644d864a1b61bc015f63633bb72fc2a36b1b8
chibiWSL <48870208+chibichibichaba@users.noreply.github.com>
2026-06-15 21:30:30 +0200

    added bahviour tree

--- Navigation/Pathfinding/Navigation/setup.py
+++ Navigation/Pathfinding/Navigation/setup.py
@@ -1,5 +1,6 @@
 from glob import glob
 from setuptools import find_packages, setup
+import os
 
 package_name = 'navigation'
 
@@ -8,12 +9,30 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
-        ('share/' + package_name + '/config', glob('config/*.yaml')),
-        ('share/' + package_name + '/rviz', glob('rviz/*.rviz')),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            'share/' + package_name + '/launch',
+            glob('launch/*.launch.py')
+        ),
+        (
+            'share/' + package_name + '/config',
+            glob('config/*.yaml')
+        ),
+        (
+            'share/' + package_name + '/rviz',
+            glob('rviz/*.rviz')
+        ),
+        (
+            os.path.join('share', package_name, 'behavior_trees'),
+            glob('behavior_trees/*.xml')
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -30,5 +49,4 @@ setup(
             'conditional_cmd_vel_limiter = navigation.conditional_cmd_vel_limiter:main',
         ],
     },
-
 )
